cbb5ae881785d3d830e53645982c1405a6423401
felipe2077 <felipebatista47@hotmail.com>
2022-05-17 15:26:31 -0300

    primeiro commit

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+let listaDados = [
+  "./images/dice1.png",
+  "./images/dice2.png",
+  "./images/dice3.png",
+  "./images/dice4.png",
+  "./images/dice5.png",
+  "./images/dice6.png",
+];
+
+function setDices() {
+  let diceOneRandon = Math.floor(Math.random() * listaDados.length);
+  let diceTwoRandon = Math.floor(Math.random() * listaDados.length);
+  let diceOne = document
+    .querySelector(".diceOne")
+    .setAttribute("src", listaDados[diceOneRandon]);
+
+  let diceTwo = document
+    .querySelector(".diceTwo")
+    .setAttribute("src", listaDados[diceTwoRandon]);
+
+  checkWinner(diceOneRandon, diceTwoRandon);
+}
+
+function checkWinner(diceOne, diceTwo) {
+  diceOneScore = 0;
+  diceTwoScore = 0;
+  aTie = 0;
+
+  if (diceOne === diceTwo) {
+    document.querySelector(".resultado").innerText = `Empate! 😐`;
+  } else if (diceOne > diceTwo) {
+    document.querySelector(
+      ".resultado"
+    ).innerText = `Vencedor: Primeiro Jogador 🥳`;
+  } else if (diceTwo > diceOne) {
+    document.querySelector(
+      ".resultado"
+    ).innerText = `Vencedor: Segundo Jogador 🥳`;
+  }
+}
